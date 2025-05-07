d6462a3667647fb4c97b610e5f70b473e5672167
Ryan <git@hexa.mozmail.com>
2025-05-06 20:00:01 -0400

    switch to docker builds

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+FROM node:23.11.0
+COPY --from=ghcr.io/astral-sh/uv:0.7.2@sha256:3b898ca84fbe7628c5adcd836c1de78a0f1ded68344d019af8478d4358417399 /uv /uvx /bin/
+
+WORKDIR /app
+ADD pyproject.toml uv.lock package.json yarn.lock  ./
+
+RUN yarn && uv sync --frozen --no-cache
+
+ADD . .
+
+RUN yarn build
+
+CMD ["yarn", "run", "start"]
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,5 @@
 // @ts-check
 import eslint from '@eslint/js';
-import vitest from '@vitest/eslint-plugin';
 import eslintConfigPrettier from 'eslint-config-prettier';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
@@ -37,29 +36,6 @@ export default tseslint.config(
 
       parserOptions: {
         project: './tsconfig.json',
-      },
-    },
-  },
-  {
-    files: ['__tests__/**'],
-
-    plugins: {
-      vitest,
-    },
-
-    rules: {
-      ...vitest.configs.recommended.rules,
-    },
-
-    settings: {
-      vitest: {
-        typecheck: true,
-      },
-    },
-
-    languageOptions: {
-      globals: {
-        ...vitest.environments.env.globals,
       },
     },
   },
